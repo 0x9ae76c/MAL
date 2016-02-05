4da4b194e5ca6dbff53bd8ab402a308efa30a9b0
0x9ae76c <17cf@cock.li>
2016-02-05 18:05:46 -0500

    demo

---
+++ shim.js
@@ -0,0 +1,3 @@
+var t = document.querySelector('#waifupane')
+t.setAttribute('style', 'top:0; left: 540px; position:absolute; border:1px solid black; display: block; width: 506px;')
+t.innerHTML = '<video autoplay loop src="http://puu.sh/mXeSc/60f442d80a.webm"></video>'
